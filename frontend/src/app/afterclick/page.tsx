7e5fadd34792cf1c38f4611f6f4db9d2fd880856
Jesse Andrade <andradejf.inbox@gmail.com>
2024-11-05 14:49:51 -0700

    tried a bunch of stuff and it sucked

---
+++ frontend/src/app/afterclick/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+    return (
+        //     popup page //
+        <>
+            <h1 className={'sm:m-0 p-4 md: items-center justify-center bg-center bg-orange-300 '}> Pop Up Static
+                Page</h1>
+
+
+
+        </>
+
+    )
+}
--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,5 +1,8 @@
+
+
 export default function NotFound() {
     return (
         <h1>This is not the page you are looking for</h1>
+
     )
 }
