2479427162e796e22f91b829d1f453f85a3f360f
Jesse Andrade <andradejf.inbox@gmail.com>
2024-11-05 19:46:11 -0700

    tried to get popup working

--- frontend/src/app/afterclick/page.tsx
+++ frontend/src/app/afterclick/page.tsx
@@ -1,13 +1,59 @@
-export default function Page() {
+
+"use client";
+
+import { Button, Modal, Select } from "flowbite-react";
+import { useState } from "react";
+
+export function Component() {
+    const [openModal, setOpenModal] = useState(true);
+    const [modalPlacement, setModalPlacement] = useState('center')
+
     return (
-        //     popup page //
         <>
-            <h1 className={'sm:m-0 p-4 md: items-center justify-center bg-center bg-orange-300 '}> Pop Up Static
-                Page</h1>
-
-
-
+            <div className="flex flex-wrap gap-4">
+                <div className="w-40">
+                    <Select defaultValue="center" onChange={(event) => setModalPlacement(event.target.value)}>
+                        <option value="center">Center</option>
+                        <option value="top-left">Top left</option>
+                        <option value="top-center">Top center</option>
+                        <option value="top-right">Top right</option>
+                        <option value="center-left">Center left</option>
+                        <option value="center-right">Center right</option>
+                        <option value="bottom-right">Bottom right</option>
+                        <option value="bottom-center">Bottom center</option>
+                        <option value="bottom-left">Bottom left</option>
+                    </Select>
+                </div>
+                <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
+            </div>
+            <Modal
+                show={openModal}
+                position={modalPlacement}
+                onClose={() => setOpenModal(false)}
+            >
+                <Modal.Header>Small modal</Modal.Header>
+                <Modal.Body>
+                    <div className="space-y-6 p-6">
+                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
+                            companies around the world are updating their terms of service agreements to comply.
+                        </p>
+                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
+                            to ensure a common set of data rights in the European Union. It requires organizations to notify users as
+                            soon as possible of high-risk data breaches that could personally affect them.
+                        </p>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={() => setOpenModal(false)}>I accept</Button>
+                    <Button color="gray" onClick={() => setOpenModal(false)}>
+                        Decline
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
-
-    )
+    );
 }
+
+
