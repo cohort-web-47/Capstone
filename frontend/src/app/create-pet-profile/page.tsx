a057f94b4a134dfb48d9c2658cddbf8677f2f96e
Jesse Andrade <andradejf.inbox@gmail.com>
2024-11-06 16:48:39 -0700

    added dropdown menus

--- frontend/src/app/create-pet-profile/page.tsx
+++ frontend/src/app/create-pet-profile/page.tsx
@@ -8,7 +8,21 @@ export default function () {
                 <img className="rounded-full w-36 h-36 mx-auto" src={"https://placehold.co/400"} alt="placeholder"/>
             </div>
             <form className="flex max-w-md flex-col gap-4 mx-auto">
-                <h3>Create Pet Profile</h3>
+                <div>
+                    <h3>Create Pet Profile</h3>
+
+                    <div className="w-36">
+                        <div className="mb-2 block">
+                            <Label htmlFor="SelectTypeofPet" value="Select Type of Pet"/>
+                        </div>
+                        <Select id="Selected Pet" required>
+                            <option>Pet Type</option>
+                            <option>Cat</option>
+                            <option>Dog</option>
+                        </Select>
+                    </div>
+
+                </div>
                 <div>
                     <div className="mb-2 block">
                         <Label htmlFor="petname" value="Pet Name"/>
@@ -37,7 +51,7 @@ export default function () {
                     <div className="mb-2 block">
                         <Label htmlFor="Personality" value="Personality of your dog"/>
                     </div>
-                    <Select id="Personality" required >
+                    <Select id="Personality" required>
                         <option>Personality</option>
                         <option>Calm</option>
                         <option>Playful</option>
@@ -48,6 +62,55 @@ export default function () {
 
                 <Button type="submit"> Edit Profile | Save </Button>
             </form>
+
+<br/><br/><br/>
+
+            <div>
+                <img className="rounded-full w-36 h-36 mx-auto" src={"https://placehold.co/400"} alt="placeholder"/>
+            </div>
+            <form className="flex max-w-md flex-col gap-4 mx-auto">
+                <h3>Create Pet Profile</h3>
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="petname" value="Pet Name"/>
+                    </div>
+                    <TextInput id="petname" type="Pet Name" placeholder='"Pet Name"' required shadow/>
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="CatBreed" value="Cat Breed"/>
+                    </div>
+                    <TextInput id="CatBreed" type="CatBreed" placeholder='"Cat Breed"' required shadow/>
+                </div>
+                <div className="max-w-md">
+                    <div className="mb-2 block">
+                        <Label htmlFor="Cat In/Out" value="Inside or Outside"/>
+                    </div>
+                    <Select id="Type of Cat" required>
+                        <option>Indoor/Outdoor</option>
+                        <option>Inside</option>
+                        <option>Outside</option>
+                        <option>In & Out</option>
+                    </Select>
+                </div>
+                <div className="max-w-md">
+                    <div className="mb-2 block">
+                        <Label htmlFor="Personality" value="Personality of your dog"/>
+                    </div>
+                    <Select id="Personality" required>
+                        <option>Personality</option>
+                        <option>Fancy</option>
+                        <option>Calm</option>
+                        <option>Playful</option>
+                        <option>Excited</option>
+                        <option>Snooty</option>
+                        <option>Loner</option>
+                    </Select>
+                </div>
+
+                <Button type="submit"> Edit Profile | Save </Button>
+            </form>
+
 
         </>
     )
