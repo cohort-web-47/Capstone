127e4179fbfe5b3d3f873b09ab0058bdc813d08e
Patrick Day <patday30@gmail.com>
2024-11-06 14:19:53 -0700

    added search page

--- frontend/src/app/(index)/page.tsx
+++ frontend/src/app/(index)/page.tsx
@@ -1,5 +1,14 @@
+
 export default function Home() {
+
+
     return (
-        <h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>
-    )
-}
+        <>
+            <h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>
+            <a href='/search'>
+                <button className="bg-font w-20 h-20">Search</button>
+            </a>
+
+       </>
+            )
+     }
