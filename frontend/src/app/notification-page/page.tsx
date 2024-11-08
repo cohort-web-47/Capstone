5e652cfa87efe49fb31e995f165d9ac15ea88105
Jesse Andrade <andradejf.inbox@gmail.com>
2024-11-07 21:34:59 -0700

    made changes to notifications page

--- frontend/src/app/notification-page/page.tsx
+++ frontend/src/app/notification-page/page.tsx
@@ -5,17 +5,127 @@ import Image from "next/image";
 export default function () {
     return (
         <>
-
-            <div className="w-1/2 h-20 bg- rounded-xl my-6 self-center flex items-center gap-6 md:w-3/4">
-                <div className={"rounded-full w-12 h-12 text-center"}>
-                    <img className="rounded-full w-12 h-12" src={"https://picsum.photos/200/300"} alt="profile picture"/>
+            <br/>
+            <div>
+                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                    <div className=" rounded-full w-12 h-12 text-center">
+                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                             alt="profile picture"/>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <p>Profile Name</p>
+                        <p>likes your post</p>
+                    </div>
                 </div>
-                <p className={"justify-center"}>Profile Name like's you post</p>
             </div>
+            <hr/>
+            <div>
+                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                    <div className=" rounded-full w-12 h-12 text-center">
+                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                             alt="profile picture"/>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <p>Profile Name</p>
+                        <p>likes your post</p>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div>
+                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                    <div className=" rounded-full w-12 h-12 text-center">
+                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                             alt="profile picture"/>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <p>Profile Name</p>
+                        <p>likes your post</p>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div>
+                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                    <div className=" rounded-full w-12 h-12 text-center">
+                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                             alt="profile picture"/>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <p>Profile Name</p>
+                        <p>likes your post</p>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div>
+                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                    <div className=" rounded-full w-12 h-12 text-center">
+                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                             alt="profile picture"/>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <p>Profile Name</p>
+                        <p>likes your post</p>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div>
+                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                    <div className=" rounded-full w-12 h-12 text-center">
+                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                             alt="profile picture"/>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <p>Profile Name</p>
+                        <p>likes your post</p>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div>
+                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                    <div className=" rounded-full w-12 h-12 text-center">
+                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                             alt="profile picture"/>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <p>Profile Name</p>
+                        <p>likes your post</p>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div>
+                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                    <div className=" rounded-full w-12 h-12 text-center">
+                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                             alt="profile picture"/>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <p>Profile Name</p>
+                        <p>likes your post</p>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div>
+                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                    <div className=" rounded-full w-12 h-12 text-center">
+                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                             alt="profile picture"/>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <p>Profile Name</p>
+                        <p>likes your post</p>
+                    </div>
+                </div>
+            </div>
+            <hr/>
 
 
         </>
-
     )
 
 }
