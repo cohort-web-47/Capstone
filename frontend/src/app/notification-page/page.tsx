8d58b4baace62f34d23b493ef09de048691f9cef
Jesse Andrade <andradejf.inbox@gmail.com>
2024-11-08 23:58:22 -0700

    playing with margins

--- frontend/src/app/notification-page/page.tsx
+++ frontend/src/app/notification-page/page.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import {Card} from "flowbite-react";
 import Image from "next/image";
+import {Post} from "@/components/Post";
+import {PostProps} from "@/components/Post";
 
 export default function () {
+
     return (
         <>
             <br/>
+            <section>
             <div>
-                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                <div className="container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
+                    <div className=" rounded-full w-12 h-12 text-center">
+                        <img className=" rounded-full w-12 h-12" src="https://picsum.photos/200/300" alt="placeholder"/>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <p>Jello there</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
                     <div className=" rounded-full w-12 h-12 text-center">
                         <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
                              alt="profile picture"/>
@@ -18,9 +32,8 @@ export default function () {
                     </div>
                 </div>
             </div>
-            <hr/>
             <div>
-                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
                     <div className=" rounded-full w-12 h-12 text-center">
                         <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
                              alt="profile picture"/>
@@ -31,9 +44,8 @@ export default function () {
                     </div>
                 </div>
             </div>
-            <hr/>
             <div>
-                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
                     <div className=" rounded-full w-12 h-12 text-center">
                         <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
                              alt="profile picture"/>
@@ -44,9 +56,8 @@ export default function () {
                     </div>
                 </div>
             </div>
-            <hr/>
             <div>
-                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
                     <div className=" rounded-full w-12 h-12 text-center">
                         <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
                              alt="profile picture"/>
@@ -57,9 +68,8 @@ export default function () {
                     </div>
                 </div>
             </div>
-            <hr/>
             <div>
-                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
                     <div className=" rounded-full w-12 h-12 text-center">
                         <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
                              alt="profile picture"/>
@@ -70,9 +80,8 @@ export default function () {
                     </div>
                 </div>
             </div>
-            <hr/>
             <div>
-                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
                     <div className=" rounded-full w-12 h-12 text-center">
                         <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
                              alt="profile picture"/>
@@ -83,9 +92,8 @@ export default function () {
                     </div>
                 </div>
             </div>
-            <hr/>
             <div>
-                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
                     <div className=" rounded-full w-12 h-12 text-center">
                         <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
                              alt="profile picture"/>
@@ -96,9 +104,8 @@ export default function () {
                     </div>
                 </div>
             </div>
-            <hr/>
             <div>
-                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
+                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
                     <div className=" rounded-full w-12 h-12 text-center">
                         <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
                              alt="profile picture"/>
@@ -109,22 +116,8 @@ export default function () {
                     </div>
                 </div>
             </div>
-            <hr/>
-            <div>
-                <div className=" container flex items-center space-x-4 border-2 border-black rounded p-2">
-                    <div className=" rounded-full w-12 h-12 text-center">
-                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
-                             alt="profile picture"/>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <p>Profile Name</p>
-                        <p>likes your post</p>
-                    </div>
-                </div>
-            </div>
-            <hr/>
 
-
+            </section>
         </>
     )
 
