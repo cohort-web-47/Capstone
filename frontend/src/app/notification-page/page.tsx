8c857d5871bbaf2428f4a3383b415831941b7d46
Jesse Andrade <andradejf.inbox@gmail.com>
2024-11-09 15:37:16 -0700

    updated margins on notification design

--- frontend/src/app/notification-page/page.tsx
+++ frontend/src/app/notification-page/page.tsx
@@ -1,121 +1,240 @@
 import React from 'react'
-import {Card} from "flowbite-react";
-import Image from "next/image";
-import {Post} from "@/components/Post";
-import {PostProps} from "@/components/Post";
 
 export default function () {
+    // ask marty about building functions to populated profile name and profile image into html.
 
     return (
         <>
             <br/>
             <section>
-            <div>
-                <div className="container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
-                    <div className=" rounded-full w-12 h-12 text-center">
-                        <img className=" rounded-full w-12 h-12" src="https://picsum.photos/200/300" alt="placeholder"/>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <p>Jello there</p>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
-                    <div className=" rounded-full w-12 h-12 text-center">
-                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
-                             alt="profile picture"/>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <p>Profile Name</p>
-                        <p>likes your post</p>
+                <div>
+                    <div className="sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src="https://picsum.photos/200/300"
+                                 alt="placeholder"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
-                    <div className=" rounded-full w-12 h-12 text-center">
-                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
-                             alt="profile picture"/>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <p>Profile Name</p>
-                        <p>likes your post</p>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
-                    <div className=" rounded-full w-12 h-12 text-center">
-                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
-                             alt="profile picture"/>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <p>Profile Name</p>
-                        <p>likes your post</p>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
-                    <div className=" rounded-full w-12 h-12 text-center">
-                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
-                             alt="profile picture"/>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <p>Profile Name</p>
-                        <p>likes your post</p>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
-                    <div className=" rounded-full w-12 h-12 text-center">
-                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
-                             alt="profile picture"/>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <p>Profile Name</p>
-                        <p>likes your post</p>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
-                    <div className=" rounded-full w-12 h-12 text-center">
-                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
-                             alt="profile picture"/>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <p>Profile Name</p>
-                        <p>likes your post</p>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
-                    <div className=" rounded-full w-12 h-12 text-center">
-                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
-                             alt="profile picture"/>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <p>Profile Name</p>
-                        <p>likes your post</p>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div className=" container flex items-center space-x-4 border-2 border-gray-400 rounded p-2">
-                    <div className=" rounded-full w-12 h-12 text-center">
-                        <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
-                             alt="profile picture"/>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <p>Profile Name</p>
-                        <p>likes your post</p>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className=" sm:container md:mx-auto flex space-x-4 border-2 border-gray-400 rounded p-2">
+                        <div className=" rounded-full w-12 h-12 text-center">
+                            <img className=" rounded-full w-12 h-12" src=" https://picsum.photos/200/300"
+                                 alt="profile picture"/>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <p>Profile Name</p>
+                            <p>likes your post</p>
+                        </div>
+                    </div>
+                </div>
 
             </section>
         </>
