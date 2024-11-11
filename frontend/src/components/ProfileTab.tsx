498fa47699c386db305497837b96a8e4731ec38e
Patrick Day <patday30@gmail.com>
2024-11-11 09:50:36 -0700

    adjusted gap and margins

--- frontend/src/components/ProfileTab.tsx
+++ frontend/src/components/ProfileTab.tsx
@@ -15,8 +15,8 @@ export default function ProfileTab(props: ProfileProps) {
     return (
         <>
             <div
-                className="w-2/3 py-2 pl-6 bg-profile rounded-xl  flex items-center gap-6 cursor-pointer hover:bg-white">
-                <img className={"rounded-full w-12 h-12 cursor-pointer"} src={profile.imageUrl} alt="profile pic"/>
+                className="w-2/3 py-2 pl-6 bg-profile rounded-xl  flex items-center gap-12 cursor-pointer hover:bg-white">
+                <img className={"rounded-full w-12 h-12 mr-6 cursor-pointer"} src={profile.imageUrl} alt="profile pic"/>
 
                 <p className="justify-center">{profile.profileName}</p>
             </div>
