debdd6450d753d3387fd7463d49632edb8e4c902
Jesse Andrade <andradejf.inbox@gmail.com>
2024-11-15 13:22:53 -0700

    did things and broke it

---
+++ frontend/src/app/comments-page/CommentsPage.tsx
@@ -0,0 +1,8 @@
+export default function CommentsPage() {
+    return (
+
+<h1>Round 2</h1>
+
+    )
+
+}
---
+++ frontend/src/app/comments-page/page.tsx
@@ -0,0 +1,16 @@
+import CommentsPage from "@/app/comments-page/CommentsPage";
+import Searchbar from "@/components/Searchbar";
+import SearchPage from "@/app/search/page";
+
+
+export default function () {
+    return (
+
+        <>
+            <Searchbar/>
+
+        </>
+
+
+    )
+}
