784d5871cbff236c1478b3b7c17731294e52bf5f
Hesham Elhamdani <helhamdani@cnm.edu>
2024-11-15 08:44:31 -0700

    navagation progress

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout(props : RootLayoutProps) {
         <html  lang="en" suppressHydrationWarning>
 
         <body>
-        <Nav />
        {children}
          <Footer/>
         </body>
