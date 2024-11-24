dc0fb7a6d333c60b19dc1adc38cfb59a96bc620d
Patrick Day <patday30@gmail.com>
2024-11-24 09:46:51 -0700

    added flex-wrap

--- frontend/src/components/ProfileTab.tsx
+++ frontend/src/components/ProfileTab.tsx
@@ -15,7 +15,7 @@ export default function ProfileTab(props: ProfileProps) {
     return (
         <>
             <div
-                className="w-2/3 py-2 pl-6 bg-themeProfile rounded-xl  flex items-center gap-12 md:gap-1 md:flex-wrap lg:gap-12 xl-gap-12 cursor-pointer hover:bg-white">
+                className="w-2/3 py-2 pl-6 bg-themeProfile rounded-xl flex items-center gap-12 md:gap-1 md:flex-wrap lg:gap-12 xl-gap-12 cursor-pointer hover:bg-white">
                 <img className={"rounded-full w-12 h-12  mr-6 cursor-pointer"} src={profile.imageUrl} alt="profile pic"/>
 
                 <p className="justify-center">{profile.profileName}</p>
