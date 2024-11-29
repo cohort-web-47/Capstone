4175dc8abc02942655057c289dc2f979c5462193
Patrick Day <patday30@gmail.com>
2024-11-29 15:26:59 -0700

    getLikesByPost and getLikesByPet

--- backend/src/apis/like/like.controller.ts
+++ backend/src/apis/like/like.controller.ts
@@ -4,7 +4,16 @@ import {Status} from "../../utils/interfaces/Status";
 import {PublicProfile} from "../profile/profile.model";
 import {selectPetByPetId} from "../pet/pet.model"
 import {Request, Response} from "express";
-import {deleteLike, insertLike, Like, selectLikeByLikeId} from "./like.model";
+import {
+    deleteLike,
+    insertLike,
+    Like,
+    selectLikeByLikeId,
+    selectLikesByLikePetId,
+    selectLikesByLikePostId
+} from "./like.model";
+import {z} from "zod";
+import {selectCommentsByCommentPostId} from "../comment/comment.model";
 
 
 export async function postLikeController(request: Request, response: Response): Promise<Response<Status>> {
@@ -162,5 +171,72 @@ export async function toggleLikeController(request: Request, response: Response)
     } catch (error: any) {
         return (response.json({status: 500, data: null, message: error.message}))
 
+    }
+}
+
+
+
+// export async function getLikesByLikePostIdController(request: Request, response: Response): Promise<Response> {
+//     try {
+//
+//         // validate the likeProfileId coming from the request parameters
+//         const validationResult = z.string().uuid("Please provide a valid likeThreadId").safeParse(request.params.likePostId)
+//
+//         // if the validation fails, return a response to the client
+//         if (!validationResult.success) {
+//             return zodErrorResponse(response, validationResult.error)
+//         }
+//
+//         // if the validation succeeds, continue
+//
+//         // deconstruct the like thread id from the request parameters
+//         const likePostId = validationResult.data
+//
+//         // select the likes by like thread id
+//         const data = await selectLikesByLikePostId(likePostId)
+//
+//         // return the status and the likes associated with the thread
+//         return response.json({status: 200, message: null, data})
+//
+//         // if an error occurs, return the error to the user
+//     } catch (error) {
+//         return response.json({
+//             status: 500,
+//             message: '',
+//             data: []
+//         })
+//     }
+// }
+
+
+export async function getLikesByLikePostIdController (request: Request, response: Response): Promise<Response<Status>> {
+    try{
+        const validationResult = z.string().uuid({message: 'Please provide a valid pet CommentPostId'}).safeParse(request.params.likePostId)
+        console.log(validationResult)
+        if (!validationResult.success) {
+            return zodErrorResponse(response, validationResult.error)
+        }
+        const likePostId = validationResult.data
+        const data = await selectLikesByLikePostId(likePostId)
+        return response.json({status: 200, message: null, data})
+
+    } catch (error) {
+        return response.json ({status: 500, message: '', data: []})
+    }
+}
+
+export async function getLikesByPetIdController(request: Request, response: Response): Promise<Response<Status>> {
+    try {
+        const validationResult = z.string().uuid({message: 'Please provide a valid pet ID'}).safeParse(request.params.likePetId)
+        if (!validationResult.success) {
+            return zodErrorResponse(response, validationResult.error)
+        }
+        const likePetId = validationResult.data
+
+        const data = await selectLikesByLikePetId(likePetId)
+        return response.json({status: 200, message: null, data})
+
+    } catch (error) {
+        return (response.json({status: 500, message:'', data: []}))
     }
 }
--- backend/src/apis/like/like.model.ts
+++ backend/src/apis/like/like.model.ts
@@ -1,6 +1,8 @@
 import {LikeSchema} from "./like.validator";
 import {z} from "zod";
 import {sql} from "../../utils/database.utils";
+import {CommentSchema} from "../comment/comment.validator";
+import {Comment} from "../comment/comment.model";
 
 export type Like = z.infer<typeof LikeSchema>
 export async function insertLike(like:Like): Promise<string>{
@@ -39,3 +41,28 @@ export async function selectLikeByLikeId(like: Like): Promise<Like | null> {
     // return the like that was selected
     return result.length === 0 ? null : result[0]
 }
+
+
+
+export async function selectLikesByLikePostId(likePostId: string): Promise<Like[]>  {
+
+    // select the likes from the like table by likePostId
+    const rowList = <Like[]>await sql`SELECT like_pet_id, like_post_id, like_datetime
+                                      FROM "like"
+                                      WHERE like_post_id = ${likePostId}`
+
+    // parse the result into an array of likes and return it
+    return LikeSchema.array().parse(rowList)
+}
+
+export async function selectLikesByLikePetId(likePetId: string): Promise<Like[] | null> {
+
+    const rowList = <Like[]>await sql`SELECT like_pet_id, like_post_id, like_datetime
+                                      FROM "like"
+                                      WHERE like_pet_id = ${likePetId}`
+    return LikeSchema.array().parse(rowList)
+}
+
+// export async function selectLikeByCommentId(commentId: string): Promise<Like | null> {}
+
+
