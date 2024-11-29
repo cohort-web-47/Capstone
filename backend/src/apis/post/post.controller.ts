78a8915c3f318cee625bfa8174b2896561707a5a
Jesse Andrade <andradejf.inbox@gmail.com>
2024-11-29 09:45:15 -0700

    updated to mirrow branch to work on

--- backend/src/apis/post/post.controller.ts
+++ backend/src/apis/post/post.controller.ts
@@ -1,7 +1,8 @@
 import {Request, Response} from 'express';
 import {
+    deletePostByPostId,
     insertPost,
-    Post
+    Post, selectAllPosts, selectPostByPetId, selectPostByPostId
 
 
 } from "./post.model"
@@ -10,6 +11,7 @@ import {PostSchema} from "./post.validator";
 import {zodErrorResponse} from "../../utils/response.utils";
 import {PublicProfile} from "../profile/profile.model";
 import {Pet, selectPetByPetId} from "../pet/pet.model";
+import {z} from "zod";
 
 export async function createPostController(request: Request, response: Response): Promise<Response | undefined> {
     try {
@@ -51,5 +53,112 @@ export async function createPostController(request: Request, response: Response)
     } catch (error) {
         console.log(error)
         return response.json({status: 500, message: 'Error creating Post. Try again', data: null})
+    }
+}
+
+export async function getAllPosts (request: Request, response: Response): Promise<Response<Status>> {
+    try {
+        const data = await selectAllPosts()
+
+        const status: Status = {status: 200, message: null, data}
+        return response.json(status)
+    } catch (error) {
+        console.error(error)
+        return response.json ({
+            status: 500,
+            message: 'Error getting all posts.',
+            data: []
+        })
+    }
+}
+
+export async function getPostByPetIdController (request: Request, response: Response): Promise<Response<Status>> {
+    try {
+        const validationResult = z.string().uuid({message: 'Please provide a valid PostId'}).safeParse(request.params.petId)
+
+        if (!validationResult.success) {
+            return zodErrorResponse(response, validationResult.error)
+        }
+        console.log(getPostByPetIdController)
+
+        const petId = validationResult.data
+
+        const data = await selectPostByPetId(petId)
+
+        return response.json({status: 200, message: null, data})
+    } catch (error) {
+        return response.json({
+            status: 500,
+            message: 'post id not found',
+            data: []
+        })
+    }
+}
+export async function getPostByPostIdController (request: Request, response: Response): Promise<Response<Status>> {
+    try {
+        const validationResult = z.string().uuid({message: 'Please provide a valid Post Id'}).safeParse(request.params.postId)
+
+        if (!validationResult.success) {
+            return zodErrorResponse(response, validationResult.error)
+        }
+
+        const postId = validationResult.data
+
+        const data = await selectPostByPostId(postId)
+
+        return response.json({status: 200, message: null, data})
+    } catch (error) {
+        return response.json({
+            status: 500,
+            message: 'post id not found',
+            data: []
+        })
+    }
+}
+
+
+export async function deletePostByPostIdController (request: Request, response: Response): Promise<Response<Status>> {
+    try {
+        const validationResult = z.string().uuid({message: 'Please provide a valid PostId'}).safeParse(request.params.postId)
+
+        if (!validationResult.success) {
+            return zodErrorResponse(response, validationResult.error)
+        }
+
+        const profile: PublicProfile = request.session.profile as PublicProfile
+
+        const profileId: string = profile.profileId as string
+
+        const postId = validationResult.data
+
+        const post = await selectPostByPostId(postId)
+        if (!post){
+            return response.json({
+                status: 403,
+                message: 'No post with this ID',
+                data: null
+            })
+        }
+        const pet = await selectPetByPetId(post.postPetId)
+
+        if (pet?.petProfileId !== profileId ) {
+            return response.json({
+                status: 403,
+                message: 'You are not allowed to delete this pets post,',
+                data: null
+            })
+        }
+
+        const result = await deletePostByPostId(postId)
+
+        return response.json({status: 200, message: "You did it!", data: null})
+
+
+    } catch (error) {
+        return response.json({
+            status: 500,
+            message: '',
+            data: []
+        })
     }
 }
--- backend/src/apis/post/post.route.ts
+++ backend/src/apis/post/post.route.ts
@@ -1,14 +1,26 @@
 import { Router } from 'express';
 import {
-    createPostController
+    createPostController, deletePostByPostIdController, getAllPosts, getPostByPetIdController, getPostByPostIdController
 
 } from "./post.controller";
 import {isLoggedInController} from "../../utils/controllers/isloggedin.controller";
+
 
 const basePath = '/apis/post';
 
 const router = Router()
 
 router.route('/').post(isLoggedInController,createPostController)
+router.route('/').get(getAllPosts)
+router.route('/petId/:petId').get(getPostByPetIdController)
+router.route('/:postId')
+    .get(getPostByPetIdController)
+    .delete(
+        isLoggedInController,
+        deletePostByPostIdController
+    )
+
+
+
 
 export const postRoute = {basePath, router}
