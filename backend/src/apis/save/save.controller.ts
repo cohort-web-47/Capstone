c72692b37f406c1f0b39798839a12d68dd390eb3
Stephanie Perea <sperea10@cnm.edu>
2024-11-30 15:44:43 -0700

    finished bookmark save

--- backend/src/apis/save/save.controller.ts
+++ backend/src/apis/save/save.controller.ts
@@ -3,7 +3,7 @@ import {Status} from "../../utils/interfaces/Status";
 import {z} from "zod";
 import {zodErrorResponse} from "../../utils/response.utils";
 import {SaveSchema} from "./save.validator";
-import {insertSave, Save} from "./save.model";
+import {insertSave, Save, selectPostsBySaveProfileId} from "./save.model";
 import {PublicProfile} from "../profile/profile.model";
 
 
@@ -35,3 +35,20 @@ export async function postSaveController(request: Request, response: Response) :
     }
 }
 
+export async function getPostsBySaveProfileIdController (request: Request, response: Response): Promise<Response<Status>> {
+    try {
+        const profile = request.session.profile as PublicProfile
+        const saveProfileId: string = profile.profileId as string
+
+       const data = await selectPostsBySaveProfileId(saveProfileId)
+
+        return response.json({status: 200, message: null, data})
+    } catch (error) {
+        console.error(error)
+        return response.json({
+            status: 500,
+            message: 'post id not found',
+            data: []
+        })
+    }
+}
--- backend/src/apis/save/save.route.ts
+++ backend/src/apis/save/save.route.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import {postSaveController} from "./save.controller";
+import {getPostsBySaveProfileIdController, postSaveController} from "./save.controller";
+import {isLoggedInController} from "../../utils/controllers/isloggedin.controller";
 
 const basePath = '/apis/save'
+
 const router = Router()
 
-router.route('/').post(postSaveController)
+router.route('/').post(isLoggedInController, postSaveController)
+router.route('/saveProfileId/:saveProfileId').get(isLoggedInController, getPostsBySaveProfileIdController)
+
 
 export const saveRoute ={basePath, router};
