9783747bbaca17ae2cf62d5dfe5efd6070b71bd5
Stephanie Perea <sperea10@cnm.edu>
2024-11-30 13:37:05 -0700

    follow toggle delete select

--- backend/src/apis/follow/follow.model.ts
+++ backend/src/apis/follow/follow.model.ts
@@ -1,6 +1,8 @@
 import {z} from 'zod'
 import {FollowSchema} from "./follow.validator";
 import {sql} from "../../utils/database.utils";
+import {LikeSchema} from "../like/like.validator";
+import {Like} from "../like/like.model";
 
 export type Follow = z.infer<typeof FollowSchema>
 
@@ -15,4 +17,60 @@ export async function insertFollow(follow: Follow): Promise<string> {
 
     // return a message to the user indicating success
     return 'Follow successfully posted'
+}
+
+export async function selectFollowsByFollowerPetId(followerPetId: string): Promise<Follow[]> {
+
+    // selects a list of follows for the pet profiles that the profile is following
+    const rowList = <Follow[]>await sql`SELECT follower_pet_id, followee_pet_id
+                                        FROM follow
+                                        WHERE follower_pet_id = ${followerPetId}`
+
+    // parse the result into an array of follows and return it
+    return FollowSchema.array().parse(rowList)
+}
+
+export async function selectFollowsByFolloweePetId(followeePetId: string): Promise<Follow[]> {
+
+    // selects a list of follows for the pet profiles that the profile is following
+    const rowList = <Follow[]>await sql`SELECT followee_pet_id, follower_pet_id
+                                        FROM follow
+                                        WHERE followee_pet_id = ${followeePetId}`
+
+    // parse the result into an array of follows and return it
+    return FollowSchema.array().parse(rowList)
+}
+
+export async function deleteFollow(follow: Follow): Promise<string> {
+
+    // deconstruct the follow object
+    const {followerPetId, followeePetId} = follow
+
+    // delete the follow from the follow table
+    await sql`DELETE
+              FROM follow
+              WHERE follower_pet_id = ${followerPetId}
+                AND followee_pet_id = ${followeePetId}`
+
+
+    // return a message to the user indicating success
+    return 'Follow successfully deleted'
+}
+
+export async function selectFollowByFollowId(follow: Follow): Promise<Follow | null> {
+
+    // deconstruct the follow object
+    const {followerPetId, followeePetId} = follow
+
+    // select the follow from the follow table by followId
+    const rowList = <Follow[]>await sql`SELECT follower_pet_id, followee_pet_id
+                                        FROM follow
+                                        WHERE follower_pet_id = ${followerPetId}
+                                          AND followee_pet_id = ${followeePetId}`
+
+    // parse the result into an array of follows
+    const result = FollowSchema.array().max(1).parse(rowList)
+
+    // return the follow that was selected
+    return result.length === 0 ? null : result[0]
 }
--- backend/src/apis/follow/follow.route.ts
+++ backend/src/apis/follow/follow.route.ts
@@ -1,13 +1,21 @@
 import { Router } from 'express';
 import {
-    postFollowController
+    deleteFollowController,
+    getFollowsByFolloweePetIdController,
+    getFollowsByFollowerPetIdController,
+    postFollowController, toggleFollowController
 } from "./follow.controller";
+import {isLoggedInController} from "../../utils/controllers/isloggedin.controller";
+import {toggleLikeController} from "../like/like.controller";
 
 const basePath = '/apis/follow'
 
 const router = Router();
 
 router.route('/').post(postFollowController)
-
+router.route('/followerPetId/:followerPetId').get(getFollowsByFollowerPetIdController)
+                                            .delete(deleteFollowController)
+router.route('/followeePetId/:followeePetId').get(getFollowsByFolloweePetIdController)
+router.route('/toggle').post(isLoggedInController, toggleFollowController)
 
 export const followRoute = {basePath, router}
