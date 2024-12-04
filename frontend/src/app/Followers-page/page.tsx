f013f105f5035408e1c2afbfc61076eb96dfdfb0
Stephanie Perea <sperea10@cnm.edu>
2024-12-03 18:13:40 -0700

    fixing followers page but still need to fix layout of page borders

--- frontend/src/app/Followers-page/page.tsx
+++ frontend/src/app/Followers-page/page.tsx
@@ -1,5 +1,5 @@
 import ProfileTab from "@/components/ProfileTab";
-import {Post} from "@/components/Post";
+import {PostCard} from "@/components/PostCard";
 import {IoMdNotificationsOutline} from "react-icons/io";
 import {GoPeople} from "react-icons/go";
 import {FiHome} from "react-icons/fi";
@@ -11,14 +11,45 @@ import Searchbar from "@/components/Searchbar";
 import {Footer} from "@/components/Footer";
 
 
-export default function SearchPage() {
+export default function () {
+
 
     const posts = [
-        {postId: "1", postImageUrl: "https://picsum.photos/400", postCaption: "I love cat", postPetId: "1"},
-        {postId: "2", postImageUrl: "https://picsum.photos/400", postCaption: "I love dog", postPetId: "1"},
-        {postId: "3", postImageUrl: "https://picsum.photos/400", postCaption: "I love cow", postPetId: "1"},
-        {postId: "4", postImageUrl: "https://picsum.photos/400", postCaption: "I love swine", postPetId: "1"},
-        {postId: "5", postImageUrl: "https://picsum.photos/400", postCaption: "I love goats", postPetId: "1"},
+        {
+            postId: "1",
+            postImageUrl: "https://picsum.photos/400",
+            postCaption: "I love cat",
+            postPetId: "1",
+            postDatetime: new Date()
+        },
+        {
+            postId: "2",
+            postImageUrl: "https://picsum.photos/400",
+            postCaption: "I love dog",
+            postPetId: "1",
+            postDatetime: new Date()
+        },
+        {
+            postId: "3",
+            postImageUrl: "https://picsum.photos/400",
+            postCaption: "I love cow",
+            postPetId: "1",
+            postDatetime: new Date()
+        },
+        {
+            postId: "4",
+            postImageUrl: "https://picsum.photos/400",
+            postCaption: "I love swine",
+            postPetId: "1",
+            postDatetime: new Date()
+        },
+        {
+            postId: "5",
+            postImageUrl: "https://picsum.photos/400",
+            postCaption: "I love goats",
+            postPetId: "1",
+            postDatetime: new Date()
+        },
 
     ]
     const pets = [
@@ -32,11 +63,11 @@ export default function SearchPage() {
         {profileId: "5", imageUrl: "https://picsum.photos/100", profileName: "Lemmy"},
 
     ]
-
+    let med = {width: 'w-1/3', position: 'left-1/3'};
 
     return (
         <>
-            <Searchbar/>
+            <Searchbar med={med}/>
 
             <div className="container bg-themeBackround flex-col md:flex-row ">
 
@@ -73,12 +104,14 @@ export default function SearchPage() {
                         </button>
                     </div>
                 </div>
+
+
                 {/*MOBILE VIEW. This div is hidden when screen size is Md or larger. Display: flex when Sm*/}
 
                 <div id="mobile-view" className="h-fit w-screen bg-themeBackground flex flex-col py-20 md:hidden">
 
                     <div className="container mx-auto flex flex-col items-center pr-8">
-                        {posts.map(post => <Post post={post} pet={pets[0]} key={post.postId}/>)}
+                        {/*{posts.map(post => <PostCard post={post} pet={pets[0]} key={post.postId}/>)}*/}
                     </div>
                     <div className=" bg-themeBackground my-6 flex flex-col gap-6 items-center ">
 
@@ -90,24 +123,10 @@ export default function SearchPage() {
                     </div>
                 </div>
 
-                {/*CENTER portion of the screen when in desktop view. Hidden when screen size is Sm. Display: flex when Md or larger*/}
+                {/*Followers section portion of the screen when in desktop view. Hidden when screen size is Sm. Display: flex when Md or larger*/}
 
                 <div
                     className="middle hidden md:flex  md:bg-themeBackground md:border-2 md:border-white md:w-1/3 md:flex-col md:absolute md:overflow-auto md:top-0 md:left-1/3 md:py-20">
-
-                    <div id={"label"} className={"bg-header w-full py-4 self-center mb-10"}>
-                        <p className={"text-3xl text-center"}>Popular Post</p>
-                    </div>
-                    <div className="container w-full pr-8">
-                        {posts.map(post => <Post post={post} pet={pets[0]} key={post.postId}/>)}
-                    </div>
-
-                </div>
-
-                {/*RIGHT portion of the screen when in desktop view, Hidden when screen size is Sm. Display: flex when Md or larger*/}
-
-                <div
-                    className="right-side hidden md:flex md:w-1/3 md:h-full md:bg-themeBackground md:flex-col md:items-center md:fixed md:top-0 md:right-0">
                     <div className="w-full bg-themeBackround my-6  flex flex-col gap-6 items-center justify-center">
 
                         <p className={"text-2xl"}>Connections</p>
@@ -115,8 +134,8 @@ export default function SearchPage() {
 
                     </div>
 
-
                 </div>
+
 
             </div>
 
