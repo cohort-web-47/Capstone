2c13dacdfbd4508cd02a35735a14fcddcf7223a2
Jesse Andrade <andradejf.inbox@gmail.com>
2024-12-06 15:18:17 -0700

    updating this branch with latest and greatest

--- frontend/src/app/notification-page/page.tsx
+++ frontend/src/app/notification-page/page.tsx
@@ -12,6 +12,7 @@ export default function () {
             <br/>
             <section>
                 <div>
+                    <
                 <Notification/>
                     <Notification/>
                 </div>
