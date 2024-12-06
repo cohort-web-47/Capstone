15b29d419e9cbcd044341ead4ca32098293f7cd4
Patrick Day <patday30@gmail.com>
2024-12-06 16:48:19 -0700

    postCard border NavTwo

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -3,6 +3,10 @@ import { AiOutlineLike } from "react-icons/ai";
 import { IoIosSave } from "react-icons/io";
 import {fetchPetById} from "@/utils/models/pet/pet.action";
 import {Post} from "@/utils/models/post/post.model";
+import React from "react";
+import {HR} from "flowbite-react";
+
+
 type PostProps = {
     post:Post
 }
@@ -15,17 +19,22 @@ const pet = await fetchPetById(post.postPetId)
         <>
 
             <div className="flex p-4">
-            <img className={"rounded-full w-6 h-6 mx-4"} src={pet.petImageUrl} alt="profile picture"/>
-                <div>
+
+
+                <img className={"rounded-full w-12 h-12 mx-4"} src={pet.petImageUrl} alt="profile picture"/>
+                <div className="w-3/4 h-auto">
                     <p>{pet.petName}</p>
                     <p>{post.postCaption}</p>
-                    <img src={post.postImageUrl} alt="postpicture"/>
+                    <img src={post.postImageUrl} alt="postpicture" className="w-full h-auto"/>
                     <div className="flex justify-between bg-navbar">
                         <FaRegComment/>
                         <AiOutlineLike/>
                         <IoIosSave/>
                     </div>
                 </div>
+            </div>
+            <div>
+                <HR className="border-2 border-[#D1BBA0] ml-8"/>
             </div>
         </>
     )
