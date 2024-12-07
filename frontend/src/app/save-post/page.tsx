1186b7f3bb5ef52c4f7197b432f8d8e40d93c248
MaytrickChannel <mnithimatechak@cnm.edu>
2024-12-06 21:31:13 -0700

    add save post

--- frontend/src/app/save-post/page.tsx
+++ frontend/src/app/save-post/page.tsx
@@ -4,10 +4,11 @@ import {IoMdNotificationsOutline} from "react-icons/io";
 import {GoPeople} from "react-icons/go";
 import {FiHome} from "react-icons/fi";
 import {IoMdSearch} from "react-icons/io";
-import {FaRegBookmark} from "react-icons/fa6";
 import {CiBookmark} from "react-icons/ci";
+
+
 import Searchbar from "@/components/Searchbar";
-import {Nav} from "@/components/Nav";
+import {Footer} from "@/components/Footer";
 import {fetchAllPosts} from "@/utils/models/post/post.action";
 
 
@@ -22,10 +23,11 @@ export default async function SavePostPage() {
     //
     // ]
 
+
     const posts = await fetchAllPosts();
 
     const pets = [
-        {petId: "1", petProfileId: "1", petImageUrl: "https://picsum.photos/200", petName: "Fido"}
+        {petProfileId: "1", petId: "1", petImageUrl: "https://picsum.photos/200", petName: "Fido"}
     ]
     const profiles = [
         {profileId: "1", imageUrl: "https://picsum.photos/400", profileName: "Mittens"},
@@ -35,15 +37,15 @@ export default async function SavePostPage() {
         {profileId: "5", imageUrl: "https://picsum.photos/100", profileName: "Lemmy"},
 
     ]
-    let med = {width: 'w-1/3', position: 'right-1/3'};
-
+    let med = {width: 'w-1/3', position: 'left-1/3'};
 
     return (
         <>
+            <Searchbar med={med}/>
 
-            {/*<div className={"fixed z-10 w-screen"}><Nav/></div>*/}
             <div className="container bg-themeBackround flex-col md:flex-row ">
 
+                {/*    LEFT SIDE of the screen when in desktop view. This div is hidden when screen size small.  Display: flex when Md or larger*/}
 
                 <div
                     className="left hidden  md:flex md:flex-col bg-themeBackground md:w-1/3 md:h-full md:fixed md:top-0 md:left-0 md:pl-6 md:pt-8 md:pb-12">
@@ -76,18 +78,28 @@ export default async function SavePostPage() {
                         </button>
                     </div>
                 </div>
-                <div id="mobile-view" className="h-fit w-screen bg-themeBackground flex flex-col py-20 md:hidden">
-                    <Nav/>
+                {/*MOBILE VIEW. This div is hidden when screen size is Md or larger. Display: flex when Sm*/}
 
+                <div id="mobile-view" className="h-fit w-screen bg-themeBackground flex flex-col py-20 md:hidden">
 
                     <div className="container mx-auto flex flex-col items-center pr-8">
-                        {posts.map(post => <PostCard post={post} key={post.postId}/>)}
+                        {posts.map(post => <PostCard post={post}  key={post.postId}/>)}
                     </div>
+                    <div className=" bg-themeBackground my-6 flex flex-col gap-6 items-center ">
 
+                        <p className={"text-2xl"}>Connections</p>
+
+                        {profiles.map(profile => <ProfileTab profile={profile} key={profile.profileId}/>)}
+
+
+                    </div>
                 </div>
+
+                {/*CENTER portion of the screen when in desktop view. Hidden when screen size is Sm. Display: flex when Md or larger*/}
+
                 <div
-                    className="middle hidden md:flex border-white border-2 md:bg-themeBackground md:w-1/3 md:flex-col md:absolute md:overflow-auto md:top-0 md:left-1/3 md:py-20">
-                    <Nav/>
+                    className="middle hidden md:flex  md:bg-themeBackground md:border-2 md:border-white md:w-1/3 md:flex-col md:absolute md:overflow-auto md:top-0 md:left-1/3 md:py-20">
+
                     <div id={"label"} className={"bg-header w-full py-4 self-center mb-10"}>
                         <p className={"text-3xl text-center"}>Popular Post</p>
                     </div>
@@ -96,19 +108,15 @@ export default async function SavePostPage() {
                     </div>
 
                 </div>
-                <div
-                    className="right-side hidden md:flex md:w-1/3 md:h-svh md:bg-themeBackground md:flex-col md:items-center md:fixed md:top-0 md:right-0">
-                    <Searchbar med={med}/>
-                    <div
-                        className="w-full bg-themeBackround pr-8 flex flex-col gap-6 items-center justify-center flex-1">
-                        <div className="w-3/4 h-auto">
-                            <PostCard post={posts[2]} />
-                        </div>
 
+                {/*RIGHT portion of the screen when in desktop view, Hidden when screen size is Sm. Display: flex when Md or larger*/}
+
+                <div
+                    className="right-side hidden md:flex md:w-1/3 md:h-full md:bg-themeBackground md:flex-col md:items-center md:fixed md:top-0 md:right-0">
+                    <div className="w-full bg-themeBackround my-6  flex flex-col gap-6 items-center justify-center">
 
                         <p className={"text-2xl"}>Connections</p>
-
-                        {profiles.slice(0, 3).map(profile => <ProfileTab profile={profile} key={profile.profileId}/>)}
+                        {profiles.map(profile => <ProfileTab profile={profile} key={profile.profileId}/>)}
 
                     </div>
 
